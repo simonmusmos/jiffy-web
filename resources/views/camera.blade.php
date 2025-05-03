c678d8156fc44cccaf7ca57c61e24c5c63fa14ab
Simon Musmos <simon.musmos@livline.jp>
2025-05-03 08:59:45 +0800

    chore: redeploy heroky

--- resources/views/camera.blade.php
+++ resources/views/camera.blade.php
@@ -24,6 +24,7 @@
             navigator.serviceWorker.register("{{ asset('sw.js') }}")
                 .then(reg => console.log('Service Worker registered:', reg));
         }
+        
     </script>
 </body>
 </html>
